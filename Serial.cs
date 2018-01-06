a5ef0393e9b8a1fbd25818ab0f5c1fa7de028b6f
wangxiufeng1104 <1017845778@qq.com>
2018-01-06 15:07:00 +0800

    修改程序的退出方式

--- Serial.cs
+++ Serial.cs
@@ -548,8 +548,16 @@ namespace Serial_IAP
             {
                 C_Monitor_Thread.Abort();
             }
-            Application.Exit();
+            Environment.Exit(0);
         }
+        /****************************
+         * 应用程序的几种退出方式
+         * 1.this.Close();   只是关闭当前窗口，若不是主窗体的话，是无法退出程序的，另外若有托管线程（非主线程），也无法干净地退出；
+         * 2.Application.Exit();  强制所有消息中止，退出所有的窗体，但是若有托管线程（非主线程），也无法干净地退出；
+         * 3.Application.ExitThread(); 强制中止调用线程上的所有消息，同样面临其它线程无法正确退出的问题；
+         * 4.System.Environment.Exit(0);   这是最彻底的退出方式，不管什么线程都被强制退出，把程序结束的很干净。
+         * *******************************/
+
         private void DelSelect_Click(object sender, EventArgs e)
         {
             while(List_LoadFile.SelectedItems.Count >= 1)
